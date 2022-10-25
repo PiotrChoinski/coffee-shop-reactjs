1559ce4c3c6598cdbbd6d44ba002dc4b93350f7a
PiotrChoinski <u18_piocho_waw@technischools.com>
2022-10-25 10:54:29 +0200

    contact hover

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -23,15 +23,15 @@ const Contact = (props) => {
             {(emailState || phoneState || adressState) && <div className='contact-container'>
                 <h1>CONTACT US</h1>
                 <div className='contact'>
-                    {phoneState && <div className={`theme--${secondColor}`} onClick={call}>
+                    {phoneState && <div className={`theme theme--${secondColor} box`} onClick={call}>
                         <img src='phone-call.png' alt='phone-call pic' />
                         <h2>{phoneText}</h2>
                     </div>}
-                    {emailState && <div className={`theme--${secondColor}`} onClick={mail}>
+                    {emailState && <div className={`theme theme--${secondColor} box`} onClick={mail}>
                         <img src='mail.png' alt='email pic' />
                         <h2>{emailText}</h2>
                     </div>}
-                    {adressState && <div className={`theme--${secondColor}`} onClick={adress}>
+                    {adressState && <div className={`theme theme--${secondColor} box`} onClick={adress}>
                         <img src='address.png' alt='address pic' />
                         <h2>{adressText}</h2>
                     </div>}
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -9,17 +9,20 @@ const Products = (props) => {
     return (
         <>
             { productsState && <div className='products-container'>
-            {products.map(product => {
-                return (
-                <div className={`offer theme--${secondColor}`} key={product.id}>
-                    <img src={product.img} alt={product.name}/>
-                    <h2>{product.name}</h2>
-                    <div className='price'>
-                        <p className={product.newPrice ? 'promo' : ''}>{product.price}</p>
-                        <p>{product.newPrice}zł</p>
-                    </div>
-                </div>)
-            })};
+                <h1>OUR PRODUCTS</h1>
+                <div className='products'>
+                    {products.map(product => {
+                        return (
+                        <div className={`offer theme--${secondColor}`} key={product.id}>
+                            <img src={product.img} alt={product.name}/>
+                            <h2>{product.name}</h2>
+                            <div className='price'>
+                                <p className={product.newPrice ? 'promo' : ''}>{product.price}</p>
+                                <p>{product.newPrice}zł</p>
+                            </div>
+                        </div>)
+                    })};
+                </div>
             </div>}
         </>
     )
